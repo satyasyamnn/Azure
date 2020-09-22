71788a23fd740d06e742168da065deff0950fb3a
satyasyamnn <satyasyamnn@gmail.com>
2020-09-22 08:33:35 +0530

    VPN Gateway P2S

---
+++ 4.Networking/Certificates/P2SRootAndClientCertificateCreation.ps1
@@ -0,0 +1,22 @@
+﻿# Command to create root certificate 
+
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+# command to create child certificate 
+
+New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
+-Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" `
+-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
+
+ # Command to fetch details of certificate based on thumbprint
+
+Get-ChildItem -Path "Cert:\CurrentUser\My"
+$cert = Get-ChildItem -Path "Cert:\CurrentUser\My\BB2BE1014C07AB23EE3DFA83DE05F3C55420ED23"
+
+
